f156bd1aa2a46618152017f167364b65604f2989
yourname <tree12132002@hotmail.com>
2021-11-14 23:52:35 +0800

    Initialize project

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
+require('./routes')(app, passport)
 
 module.exports = app

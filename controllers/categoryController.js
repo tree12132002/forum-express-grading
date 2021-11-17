6b031cad8ce7899826c8680b7092c8351f22492c
yourname <tree12132002@hotmail.com>
2021-11-18 05:03:06 +0800

    Add admin categories post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,7 +1,7 @@
 const db = require('../models')
 const Category = db.Category
 
-let categoryController = {
+const categoryController = {
   getCategories: (req, res) => {
     return Category.findAll({
       raw: true,
@@ -10,6 +10,20 @@ let categoryController = {
     .then(categories => {
       return res.render('admin/categories', { categories: categories } )
     })
+  },
+
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name
+      })
+        .then((category) => {
+          res.redirect('/admin/categories')
+        })
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -41,7 +41,9 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
   app.put('/admin/restaurants/:id', authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
+  
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
 
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)

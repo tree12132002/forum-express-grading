d8c6f2897e7e984c4b7cff8ec3d760d3a54f86b4
yourname <tree12132002@hotmail.com>
2021-11-18 05:59:23 +0800

    Add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -8,7 +8,14 @@ const categoryController = {
       nest: true
     })
     .then(categories => {
-      return res.render('admin/categories', { categories: categories } )
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then((category) => {
+            return res.render('admin/categories', { categories: categories, category: category.toJSON() })
+          })
+      } else {
+        return res.render('admin/categories', { categories: categories })
+      }
     })
   },
 
@@ -22,6 +29,21 @@ const categoryController = {
       })
         .then((category) => {
           res.redirect('/admin/categories')
+        })
+    }
+  },
+
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((cateogry) => {
+              res.redirect('/admin/categories')
+            })
         })
     }
   }

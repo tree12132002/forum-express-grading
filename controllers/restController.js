2f373660294c24c53a8ad9dcb83d09e0d7d3d009
yourname <tree12132002@hotmail.com>
2021-11-20 18:40:39 +0800

    Switch add/remove favorite button on restaurants index page and add add/remove favorite button to restaurant single page

--- controllers/restController.js
+++ controllers/restController.js
@@ -32,7 +32,8 @@ const restController = {
         const data = result.rows.map(r => ({
           ...r.dataValues,
           description: r.dataValues.description.substring(0, 50),
-          categoryName: r.dataValues.Category.name
+          categoryName: r.dataValues.Category.name,
+          isFavorited: req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
         }))
         Category.findAll({
           raw: true,
@@ -56,12 +57,18 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
     })
     .then(restaurant => {
+
+      const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id) // 找出收藏此餐廳的 User
       restaurant.increment('viewCounts')
-      return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      return res.render('restaurant', {
+        restaurant: restaurant.toJSON(),
+        isFavorited: isFavorited // 將資料傳到前端
+      })
     })
   },
 

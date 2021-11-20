e61ac8af9e33c6fb92bc47c13b5bfe341e416c94
yourname <tree12132002@hotmail.com>
2021-11-21 02:33:16 +0800

    fix R05.test.js

--- tests/R05.test.js
+++ tests/R05.test.js
@@ -16,7 +16,7 @@ let mockRestaurantData = [
     address: 'address',
     opening_hours: 'opening_hours',
     description: 'test description',
-    favoritedUsers: [
+    FavoritedUsers: [
       {
         userId: 1,
       },
@@ -30,7 +30,7 @@ let mockRestaurantData = [
     opening_hours: 'opening_hours',
     description: 'description',
     categoryId: 1,
-    favoritedUsers: [],
+    FavoritedUsers: [],
   },
 ]
 
@@ -44,7 +44,7 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
       // 模擬 getUser 函式，負責取得使用者資料
       this.getUser = sinon
         .stub(helpers, 'getUser')
-        .returns({ id: 1, followings: [], favoritedRestaurants: [] })
+        .returns({ id: 1, Followings: [], FavoritedRestaurants: [] })
     })
 
     it(' GET /restaurants/top ', (done) => {
@@ -55,7 +55,7 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
           // 回應中應包含字串'Top 10 人氣餐廳'
           // 若請求路徑正確，controller 執行後，會呼叫 res.render
           // res.render 回傳的 view 樣板裡應包含字串'Top 10 人氣餐廳'
-          res.text.should.include('Top 10 人氣餐廳') 
+          res.text.should.include('Top 10 人氣餐廳')
           done()
         })
     })
@@ -78,7 +78,7 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
         // 模擬 getUser 函式，負責取得使用者資料
         this.getUser = sinon
           .stub(helpers, 'getUser')
-          .returns({ id: 1, followings: [], favoritedRestaurants: [] })
+          .returns({ id: 1, Followings: [], FavoritedRestaurants: [] })
 
         // 建立了一個模擬的 Restaurant table，裡面放入 2 間餐廳資料
         this.restaurantMock = createModelMock('Restaurant', null, mockRestaurantData)
@@ -92,14 +92,14 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
         this.favoriteMock = createModelMock('Favorite', null, mockRestaurantData, 'FavoritedUsers')
 
         // 連向模擬的 Favorite table
-        this.userController = createControllerProxy('../controllers/userController', {Favorite: this.favoriteMock})
+        this.userController = createControllerProxy('../controllers/userController', { Favorite: this.favoriteMock })
       })
 
       it(' POST favorite ', async () => {
         // 模擬 request & response
         // 對 POST /favorite/2 發出請求，夾帶 user.favoritedRestaurants
         const req = mockRequest({
-          user: { favoritedRestaurants: [] }, 
+          user: { FavoritedRestaurants: [] },
           params: { restaurantId: 2 },
         })
         const res = mockResponse()
@@ -109,7 +109,7 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
 
         // 取得餐廳排序資料
         await this.restController.getTopRestaurant(req, res)
-        
+
         // addFavorite 執行完畢後，應呼叫 res.render
         // res.render 的第 2 個參數要包含 restaurants
         // restaurant 當中的第 2 筆資料 favoritedCount 屬性的值應是 1 (被 1 個使用者加入最愛了)
@@ -120,7 +120,7 @@ describe('# R05: TOP 10 人氣餐廳 ', function () {
         // 模擬 request & response
         // 對 DELETE /favorite/1 發出請求，夾入 user.favoritedRestaurants
         const req = mockRequest({
-          user: { favoritedRestaurants: [] },
+          user: { FavoritedRestaurants: [] },
           params: { restaurantId: 1 },
         })
         const res = mockResponse()

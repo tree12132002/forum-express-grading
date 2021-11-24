8c3aaad703760cf3bcb98fcd16de7d04815ced98
yourname <tree12132002@hotmail.com>
2021-11-25 04:59:28 +0800

    DELETE /api/admin/restaurants/:id

--- services/adminService.js
+++ services/adminService.js
@@ -20,6 +20,16 @@ const adminService = {
         callback({ restaurant: restaurant.toJSON() })
       })
   },
+
+  deleteRestaurant: (req, res, callback) => {
+    return Restaurant.findByPk(req.params.id)
+      .then((restaurant) => {
+        restaurant.destroy()
+          .then((restaurant) => {
+            callback({ status: 'success', message: '' })
+          })
+      })
+  },
 }
 
 module.exports = adminService

11e6e21c9a42b5f7d5a17c314307af3ebe232198
yourname <tree12132002@hotmail.com>
2021-11-25 22:45:48 +0800

    PUT /api/admin/categories/:id

--- services/categoryService.js
+++ services/categoryService.js
@@ -31,6 +31,20 @@ const categoryService = {
         })
     }
   },
+
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: "name didn't exist" })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then((category) => {
+          category.update(req.body)
+            .then((cateogry) => {
+              callback({ status: 'success', message: 'category was successfully updated' })
+            })
+        })
+    }
+  },
 }
 
 module.exports = categoryService
